68aa7f8fec86a6a7e55410fbc4eff2e8e49a9284
Oybek <programmingiswonderful@gmail.com>
2022-01-30 11:29:02 +0500

    working with DTO

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProductDTO } from './dto/create-product.dto';
+import { UpdateProductDTO } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -6,5 +8,30 @@ export class ProductsController {
     @Get()
     getAll() {
         return "getAll"
+    }
+
+    // @Get(':id')
+    // getOneParam(@Param() params) {
+    //     return `getOne ${params.id}`
+    // }
+
+    @Get(':id')
+    getOneId(@Param('id') id: string): string {
+        return `getOneId ${id}`
+    }
+
+    @Post()
+    create(@Body() product: CreateProductDTO): string {
+        return `Title: ${product.title}, Price: ${product.price}`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): string {
+        return `Removed ${id}`
+    }
+
+    @Put(':id')
+    update(@Body() product: UpdateProductDTO, @Param('id') id: string): string {
+        return `Updated ${id}: Title: ${product.title}, Price: ${product.price}`
     }
 }
